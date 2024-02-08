3fffb98eea953188b78f458690ca0af0b6be5251
alexhsieh <alexhsieh@awoo.com.tw>
2024-02-09 03:29:51 +0800

    update ch14

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,6 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-
 import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
@@ -12,7 +11,6 @@ import {
 } from '@/app/ui/skeletons';
 
 export default async function Page() {
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,12 +4,10 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-
 
   // function handleSearch(term: string){
   const handleSearch = useDebouncedCallback((term) => {
